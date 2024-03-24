7bbf0a6964944791922b67238007d6928735d3ca
saitogo555 <contact@saitogo.me>
2024-03-24 22:58:31 +0900

    chore: webpackの設定を削除

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
   reactStrictMode: false,
   output: "export",
   assetPrefix: "./",
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp3)$/,
-      type: "asset/resource",
-      generator: {
-        filename: "static/chunks/[path][name].[hash][ext]",
-      },
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
